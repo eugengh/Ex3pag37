40497c49e9cf24624eb50a6e8cacf6591690335a
eugengh <70998165+eugengh@users.noreply.github.com>
2021-10-11 10:03:28 +0300

    Add files via upload

---
+++ ex3pag37.py
@@ -0,0 +1,4 @@
+nume_prenume = input("Care este nunmele si prenumele tau? ")
+profesia = input ("Ce doresti sa devii in viitor(profesia)? ")
+print("Te numesti", nume_prenume)
+print("Doresti sa devii", profesia)
